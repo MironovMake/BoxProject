4974e64ac66ac891fa6cb3d18a9676c1ce790a7f
Mironov <surka2012@yandex.ru>
2021-02-12 02:51:37 +0700

    try use sleep mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,126 @@
-#include <Arduino.h>
-// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
-#include "RTClib.h"
+#include "Arduino.h"
+#include <avr/sleep.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
 
-RTC_DS1307 rtc;
+// Blink Before sleeping
+#define LED_PIN (13)
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-bool flag = 0;
-#include <Servo.h>
-int servopin = 9;
-int servoSupply = 6;
-Servo myservo; // create servo object to control a servo
-int timevar;
-int potpin = 0; // analog pin used to connect the potentiometer
+// This variable is made volatile because it is changed inside
+// an interrupt function
+volatile int f_wdt = 1;
 
-void setup()
+// Watchdog Interrupt Service. This is executed when watchdog timed out.
+ISR(WDT_vect)
 {
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-  pinMode(servoSupply, OUTPUT);
-  digitalWrite(servoSupply, HIGH);
-  myservo.attach(servopin);
-  myservo.write(15);
-  delay(2000);
-  myservo.detach();
-#ifndef ESP8266
-  while (!Serial)
-    ; // wait for serial port to connect. Needed for native USB
-#endif
-
-  if (!rtc.begin())
+  if (f_wdt == 0)
   {
-    abort();
+    // here we can implement a counter the can set the f_wdt to true if
+    // the watchdog cycle needs to run longer than the maximum of eight
+    // seconds.
+    f_wdt = 1;
   }
-
-  if (!rtc.isrunning())
-  {
-    //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    delay(3000);
-    DateTime now = rtc.now();
-    timevar = now.day();
-  }
-
-  digitalWrite(servoSupply, LOW);
 }
 
+// Enters the arduino into sleep mode.
+void enterSleep(void)
+{
+  // There are five different sleep modes in order of power saving:
+  // SLEEP_MODE_IDLE - the lowest power saving mode
+  // SLEEP_MODE_ADC
+  // SLEEP_MODE_PWR_SAVE
+  // SLEEP_MODE_STANDBY
+  // SLEEP_MODE_PWR_DOWN - the highest power saving mode
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+
+  // Now enter sleep mode.
+  sleep_mode();
+
+  // The program will continue from here after the WDT timeout
+
+  // First thing to do is disable sleep.
+  sleep_disable();
+
+  // Re-enable the peripherals.
+  power_all_enable();
+}
+
+// Setup the Watch Dog Timer (WDT)
+void setupWatchDogTimer()
+{
+  // The MCU Status Register (MCUSR) is used to tell the cause of the last
+  // reset, such as brown-out reset, watchdog reset, etc.
+  // NOTE: for security reasons, there is a timed sequence for clearing the
+  // WDE and changing the time-out configuration. If you don't use this
+  // sequence properly, you'll get unexpected results.
+
+  // Clear the reset flag on the MCUSR, the WDRF bit (bit 3).
+  MCUSR &= ~(1 << WDRF);
+
+  // Configure the Watchdog timer Control Register (WDTCSR)
+  // The WDTCSR is used for configuring the time-out, mode of operation, etc
+
+  // In order to change WDE or the pre-scaler, we need to set WDCE (This will
+  // allow updates for 4 clock cycles).
+
+  // Set the WDCE bit (bit 4) and the WDE bit (bit 3) of the WDTCSR. The WDCE
+  // bit must be set in order to change WDE or the watchdog pre-scalers.
+  // Setting the WDCE bit will allow updates to the pre-scalers and WDE for 4
+  // clock cycles then it will be reset by hardware.
+  WDTCSR |= (1 << WDCE) | (1 << WDE);
+
+  /**
+	 *	Setting the watchdog pre-scaler value with VCC = 5.0V and 16mHZ
+	 *	WDP3 WDP2 WDP1 WDP0 | Number of WDT | Typical Time-out at Oscillator Cycles
+	 *	0    0    0    0    |   2K cycles   | 16 ms
+	 *	0    0    0    1    |   4K cycles   | 32 ms
+	 *	0    0    1    0    |   8K cycles   | 64 ms
+	 *	0    0    1    1    |  16K cycles   | 0.125 s
+	 *	0    1    0    0    |  32K cycles   | 0.25 s
+	 *	0    1    0    1    |  64K cycles   | 0.5 s
+	 *	0    1    1    0    |  128K cycles  | 1.0 s
+	 *	0    1    1    1    |  256K cycles  | 2.0 s
+	 *	1    0    0    0    |  512K cycles  | 4.0 s
+	 *	1    0    0    1    | 1024K cycles  | 8.0 s
+	*/
+  WDTCSR = (1 << WDP3) | (0 << WDP2) | (0 << WDP1) | (1 << WDP0);
+  // Enable the WD interrupt (note: no reset).
+  WDTCSR |= _BV(WDIE);
+}
+
+// Setup for the serial comms and the other things
+void setup()
+{
+
+      Serial.begin(9600);
+  Serial.println("Initialising...");
+  delay(100);
+  pinMode(LED_PIN, OUTPUT);
+  setupWatchDogTimer();
+  Serial.println("Initialisation complete.");
+  delay(100);
+}
+
+// main loop
 void loop()
 {
-  delay(3000);
-  DateTime now = rtc.now();
-  timevar = now.day();
-  if (flag == 0 && timevar == 14)
+  // Wait until the watchdog have triggered a wake up.
+  if (f_wdt != 1)
   {
-    digitalWrite(servoSupply, HIGH);
-    myservo.attach(servopin);
-    myservo.write(90);
-    delay(1000);
-    myservo.detach();
-    digitalWrite(servoSupply, LOW);
-    flag = 1;
+    return;
   }
+
+  // Toggle the LED on
+  digitalWrite(LED_PIN, 1);
+  // wait
+  delay(1000);
+  // Toggle the LED off
+  digitalWrite(LED_PIN, 0);
+
+  // clear the flag so we can run above code again after the MCU wake up
+  f_wdt = 0;
+
+  // Re-enter sleep mode.
+  enterSleep();
 }
